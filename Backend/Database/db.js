7af9f24cf0e982932f8580f789d0df91882f410c
Akshay Gengaje <39873272+Akshay-Gengaje@users.noreply.github.com>
2023-07-14 10:10:24 +0530

    Update db.js

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -8,26 +8,10 @@ const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URL);
     console.log("Connected to the database");
-    await seedData();
     return "connected"
   } catch (error) {
     console.error("Failed to connect to the database:", error);
     return "Failed To Connect"
-  }
-};
-
-// Data initialized in DB with third party api
-const seedData = async () => {
-  try {
-    await Product.deleteMany({});
-    const response = await axios.get(
-      "https://s3.amazonaws.com/roxiler.com/product_transaction.json"
-    );
-    const data = response.data;
-    await Product.insertMany(data);
-    console.log("Seed data inserted into the database");
-  } catch (error) {
-    console.error("Error seeding data:", error);
   }
 };
 
